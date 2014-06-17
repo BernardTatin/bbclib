b46ed78a686b2dccf20978c266fe2a22b0427b52
WinBeefH1 <bernard.beefheart@outlook.fr>
2014-06-18 01:44:59 +0200

    Amélioration importante (simplification) du code de simple-queue, tests avec un projet Watcom.

--- structures/simple-queue.h
+++ structures/simple-queue.h
@@ -1,6 +1,5 @@
 /**
- * @file simple-queue.h 
- * (project: asu-terminal)
+ * @file simple-queue.h
  *
  * @date 17 juin 2014
  * @author bbfh
@@ -16,118 +15,128 @@
 #include <mutex>
 #endif
 
+template <class T>
+class Element {
+    private:
+        T *_value;
+    public:
+        Element(T *value) : _value(value) {
+        }
+
+        inline T *value() {
+            return _value;
+        }
+};
+
 template<class T>
-class SQElement {
-	private:
-		T *_next;
-		T *_prev;
-	protected:
+class SQElement : public Element<T> {
+    private:
+        SQElement<T> *_next;
+    public:
+        SQElement(T *value) : Element<T>(value), _next(NULL) {
+        }
 
-	public:
-		SQElement() :
-			_next(NULL), _prev(NULL) {
-
-			}
-		inline T *next() {
-			return _next;
-		}
-		inline T *prev() {
-			return _prev;
-		}
-		inline void set_next(T *next) {
-			_next = next;
-		}
-		inline void set_prev(T *prev) {
-			_prev = prev;
-		}
+        inline SQElement<T> *next() {
+            return _next;
+        }
+        inline void set_next(SQElement<T> *next) {
+            _next = next;
+        }
 };
 
 template<class T>
 class SimpleQueue {
-	protected:
-		T *first;
-		T *last;
-	public:
+    private:
+        SQElement<T> *_input;
+        SQElement<T> *_output;
+        void _push(SQElement<T> *prt) {
+            if (is_empty()) {
+                _input = prt;
+                _output = prt;
+            } else {
+                _input->set_next(prt);
+                _input = prt;
+            }
+        }
+        SQElement<T> *_pop(void) {
+            if (is_empty()) {
+                return NULL;
+            } else {
+                SQElement<T> *prt = _output;
+                _output = _output->next();
+                if (_output == NULL) {
+                    _input = NULL;
+                }
+                return prt;
+            }
+        }
+    public:
 
-		SimpleQueue() :
-#if defined(__with_threads)
-			_mtx(new std::mutex()), 
-#endif
-			first(NULL), 
-			last(NULL) {
-			}
+        SimpleQueue() :
+            _input(NULL),
+            _output(NULL) {
+            }
 
-		inline bool is_empty(void) {
-			return first == NULL || last == NULL;
-		}
-
-		void push(T *prt) {
-			if (is_empty()) {
-				first = prt;
-				last = prt;
-			} else {
-				first->set_prev(prt);
-				prt->set_next(first);
-				first = prt;
-			}
-		}
+        inline bool is_empty(void) {
+            return _input == NULL || _output == NULL;
+        }
 
 
-		T *pop(void) {
-			if (is_empty()) {
-				return NULL;
-			} else {
-				T *prt = last;
-				last = last->prev();
-				if (last != NULL) {
-					last->set_next(NULL);
-				} else {
-					first = NULL;
-				}
-				return prt;
-			}
-		}
+        inline void push(T *t) {
+            _push(new SQElement<T>(t));
+        }
 
+        inline T *pop() {
+            SQElement<T> *prt = _pop();
+            if (prt == NULL) {
+                return NULL;
+            } else {
+                T *r = new T(*(prt->value()));
+                delete prt->value();
+                delete prt;
+                return r;
+            }
+        }
 };
 
 #if defined(__with_threads)
 template<class T>
 class SimpleQueueMT : public SimpleQueue<T> {
-	private:
-		std::mutex *_mtx;
-	public:
-		SimpleQueueMT() : SimpleQueue(), _mtx(new std::mutex()) {
-		}
+    private:
+        std::mutex *_mtx;
+    public:
+        SimpleQueueMT() : SimpleQueue(), _mtx(new std::mutex()) {
+        }
 
-		bool is_empty(void) {
-			bool r;
-			lock_liste();
-			r = SimpleQueue::is_empty();
-			unlock_liste();
-			return r;
-		}
+        bool is_empty(void) {
+            bool r;
+            lock_liste();
+            r = SimpleQueue::is_empty();
+            unlock_liste();
+            return r;
+        }
 
-		void push(T *prt) {
-			lock_liste();
-			SimpleQueue::push(prt);
-			unlock_liste();
-		}
+        void push(T *prt) {
+            lock_liste();
+            SimpleQueue::push(prt);
+            unlock_liste();
+        }
 
-		T *pop(void) {
-			T *prt;
-			lock_liste();
-			prt = SimpleQueue::pop();
-			unlock_liste();
-			return prt;
-		}
+        T *pop(void) {
+            T *prt;
+            lock_liste();
+            prt = SimpleQueue::pop();
+            unlock_liste();
+            return prt;
+        }
 
-		inline void lock_liste(void) {
-			_mtx->lock();
-		}
+        inline void lock_liste(void) {
+            _mtx->lock();
+        }
 
-		inline void unlock_liste(void) {
-			_mtx->unlock();
-		}
+        inline void unlock_liste(void) {
+            _mtx->unlock();
+        }
 }
 #endif
 
---
+++ tests/test-structures.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file tests-structures.cpp
+ *
+ * @date 17 juin 2014
+ * @author bbfh
+ *
+ * @section description description
+ * description du contenu du fichier
+ */
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+#include "simple-queue.h"
+
+using namespace std;
+
+SimpleQueue<string> queue;
+
+void add_string(void) {
+    static int cpt = 0;
+    char buffer[128];
+
+    sprintf(buffer, "ligne %d", cpt++);
+    queue.push(new string(buffer));
+}
+
+int main (int argc, char **argv) {
+    queue =  SimpleQueue<string>();
+
+    for (int i=0; i<256; i++) {
+        add_string();
+    }
+    string *str = queue.pop();
+    while (str != NULL) {
+        cout << str->c_str() << endl;
+        str = queue.pop();
+    }
+#if defined(__WATCOMC__)
+    getchar();
+#endif
+}
+
