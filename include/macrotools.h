76ac634709f7c8721088fe8fe247a6a772b70d86
BernardTatin <bernard.tatin@outlook.fr>
2016-10-03 00:58:02 +0200

    Ajout de macrotools.h

---
+++ include/macrotools.h
@@ -0,0 +1,12 @@
+/*
+ * macrotools.h
+ * tools for macro
+ */
+
+#if !defined(__macrotools_h__)
+#define __macrotools_h__
+
+#	define _mac_to_str(s)		#s
+#	define mac_to_str(s)		_mac_to_str(s)
+
+#endif
