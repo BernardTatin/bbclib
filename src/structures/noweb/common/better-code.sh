4b1166f726248484afac7c7eff46f02b78dbbbff
Bernard Tatin <bernard.tatin@outlook.fr>
2017-11-04 04:19:09 +0100

    A better presentation with noweb.sty

--- src/structures/noweb/common/better-code.sh
+++ src/structures/noweb/common/better-code.sh
@@ -2,7 +2,7 @@
 
 
 while [[ $# -ne 0 ]]; do
-  sed 's/^@.*$/&\n\\noindent\\makebox[\\linewidth]{\\rule{\\paperwidth}{0.4pt}}/' $1
-  sed 's/^<<.*>>=.*$/\\noindent\\makebox[\\linewidth]{\\rule{\\paperwidth}{0.4pt}}\n&/' $1
+  sed 's/^@.*$/&\n\\noindent\\makebox[\\linewidth]{\\rule{\\textwidth}{0.4pt}}/' $1
+  sed 's/^<<.*>>=.*$/\\noindent\\makebox[\\linewidth]{\\rule{\\textwidth}{0.4pt}}\n&/' $1
   shift
 done
