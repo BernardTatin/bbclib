0f627a6b717ec4ffafafb05d4730138141f6e596
Bernard Tatin <bernard.tatin@outlook.fr>
2018-05-09 19:56:55 +0200

    forgetten commit

--- mk/bc5.mk
+++ mk/bc5.mk
@@ -9,14 +9,14 @@ LD = wine $(BC5_BASE)/bin/tlink32.exe
 TDUMP = wine $(BC5_BASE)/bin/tdump.exe
 
 CCNAME = bc5
-DEBUG = 
+DEBUG =
 MACHINE = -5 -O2 -p -a -Oc -Oi -OS -wcln -wsig -wucp -wbbf -wpin -wnak -wdef -wnod -wamb -wuse -wstv -wasm -wamp -wobs -wpch -WC -H-
 AR = wine $(BC5_BASE)/bin/tlib.exe
 _exe = .exe
 toclean = *.map $(compiler).lk1 *.BAK
 
 RUNNER = wine
-STD_WARNINGS = 
+STD_WARNINGS =
 STD_INCLUDE = -Isrc/include -Isrc/tests -Isrc/structures -Isrc -I$(WIN_BC5_BASE)/include
 STD_DEFINES = -Dcompiler=$(CCNAME) -Dno_c11
 ALLFLAGS = $(subst /,\\,$(STD_INCLUDE)) $(STD_WARNINGS) $(DEBUG) $(MACHINE) $(STD_DEFINES)
